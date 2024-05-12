69e86340fd738d93d0427fb3dce5543bc95704e2
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2024-05-13 07:39:54 +0800

    Teleop: Launch doesn't allow teleop, just submitted a proposal that didn't work  and comments

--- diff_robot_dan_teleop/launch/keyboard_teleop.launch.py
+++ diff_robot_dan_teleop/launch/keyboard_teleop.launch.py
@@ -1,24 +1,36 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    print("Not working as launch doesn't read keys...\For now, just use:")
+    print("ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/diff_dan_robot_controller/cmd_vel_unstamped")
 
     teleop_keyboard_node = Node(
-            package='teleop_twist_keyboard',
-            executable='teleop_twist_keyboard',
-            name='teleop_twist_keyboard',
-            parameters=[{'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel','/diff_dan_robot_controller/cmd_vel_unstamped')]
+            package="teleop_twist_keyboard",
+            executable="teleop_twist_keyboard",
+            output='screen',
+            #prefix = 'xterm -e',
+            name="teleop_twist_keyboard",
+            remappings=[('cmd_vel','/diff_dan_robot_controller/cmd_vel_unstamped')]
          )
+    
+    teleop_keyboard_cpp = Node(
+            package="teleop_cpp_ros2",
+            executable="teleop",
+            output='screen',
+            emulate_tty=True,
+            #prefix = 'xterm -e',
+            name="teleop_key",
+            remappings=[('cmd_vel','/diff_dan_robot_controller/cmd_vel_unstamped')]
+         )
+    
+    #teleop_process = ExecuteProcess(
+    #    cmd=[["ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/diff_dan_robot_controller/cmd_vel_unstamped"]]
+    #)
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-        
+    return LaunchDescription([        
         teleop_keyboard_node,
+        # teleop_process,
     ])
